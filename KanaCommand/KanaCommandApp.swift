7fa1683cf92c4cb856acc07f80de316d3e0035f2
Hidemune Takahashi <h1d3mun3@gmail.com>
2026-03-11 00:27:17 +0900

    Initial Commit

---
+++ KanaCommand/KanaCommandApp.swift
@@ -0,0 +1,17 @@
+//
+//  KanaCommandApp.swift
+//  KanaCommand
+//
+//  Created by hidemune on 3/11/26.
+//
+
+import SwiftUI
+
+@main
+struct KanaCommandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KanaCommandTests/KanaCommandTests.swift
@@ -0,0 +1,17 @@
+//
+//  KanaCommandTests.swift
+//  KanaCommandTests
+//
+//  Created by hidemune on 3/11/26.
+//
+
+import Testing
+@testable import KanaCommand
+
+struct KanaCommandTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
